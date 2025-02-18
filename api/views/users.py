328ada6013ba747a6d2aac4be0274cc30c6ee4fe
irfiacre <irfiacre@gmail.com>
2025-02-18 22:43:29 +0200

    Add crud for users

--- api/views/users.py
+++ api/views/users.py
@@ -21,13 +21,24 @@ def register_user_view(request):
         return Response(err, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return Response(result, status=status.HTTP_201_CREATED)
 
-
-@api_view(["GET"])
+@api_view(["GET", "PATCH", "DELETE"])
 def find_user_view(request, pk):
     try:
         result = Users.objects.get(email=pk)
         user_info = UsersSerializer(result)
     except user_info.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    return Response(user_info.data, status=status.HTTP_200_OK)
+
+    if request.method == 'GET':
+        return Response(user_info.data)
+
+    if request.method == 'PATCH':
+        serializer = UsersSerializer(result, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        result.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
