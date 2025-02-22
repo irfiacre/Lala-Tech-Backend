9827e217584dbf5fdf9d0b85cfd501dfc52d48a0
irfiacre <irfiacre@gmail.com>
2025-02-22 06:38:00 +0200

    Improve analytics

--- api/views/analytics.py
+++ api/views/analytics.py
@@ -6,27 +6,21 @@ from api.models import Properties, Booking
 
 @api_view(["GET"])
 def analytics(request, user_id):
-    # Get properties owned by the user
     user_properties = Properties.objects.filter(host_id=user_id)
 
-    # Count of properties owned by the user
     properties_count = user_properties.count()
 
-    # Get only bookings related to the user's properties
     user_property_ids = user_properties.values_list("property_id", flat=True)  # Get property IDs
     user_bookings = Booking.objects.filter(property_id__in=user_property_ids)
-
-    # Sum of booking prices for the user's properties
     total_booking_price = user_bookings.aggregate(total_price=Sum("price"))["total_price"] or 0
+    
 
-    # Count of each property's occurrence in bookings
     property_booking_counts = (
         user_bookings.values("property")
         .annotate(bookings_count=Count("property_id"))
         .order_by("-bookings_count")
     )
 
-    # Formatting property booking counts
     formatted_property_counts = {
         entry["property"]: entry["bookings_count"] for entry in property_booking_counts
     }
@@ -35,7 +29,7 @@ def analytics(request, user_id):
         {
             "properties": properties_count,
             "earnings": total_booking_price,
-            "guests": formatted_property_counts,
+            "guests": sum(formatted_property_counts.values()),
         },
         status=status.HTTP_200_OK,
     )
